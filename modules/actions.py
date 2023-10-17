afd2058d255f2e13e7c207e56d83534983da68b5
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2023-10-17 08:57:43 -0500

    add edit_video

--- modules/actions.py
+++ modules/actions.py
@@ -1,3 +1,11 @@
+import os
+import time
+from moviepy.editor import VideoFileClip
+import librosa
+import librosa.display
+from modules import actions
+from tkinter import messagebox
+
 def getTimeEnd(y, sr)-> tuple[tuple,int]:
     inner = 0
     corteArr = []
@@ -25,7 +33,7 @@ def getTimeStart(y, sr) ->  tuple[list, int]:
             return [corteArrStart,startCorte]
 
 
-def fillTemplate(template: str,newTemplate: str, values: tuple):
+def fill_template(template: str,newTemplate: str, values: tuple):
     for line in template.readlines():
         if '{{name-video}}' in line:
             name = line.replace('{{name-video}}', values.get('videoName'))
@@ -42,9 +50,82 @@ def fillTemplate(template: str,newTemplate: str, values: tuple):
         elif '{{tickStartAudio}}' in line:
             name = line.replace('{{tickStartAudio}}', values.get('tiksStart'))
             newTemplate.write(f"{name}")
+        elif '{{miniatureScale}}' in line:
+            name = line.replace('{{miniatureScale}}', values.get('miniatureScale'))
+            newTemplate.write(name)
+        elif '{{miniatureHorizontalPosition}}' in line:
+            name = line.replace('{{miniatureHorizontalPosition}}', values.get('miniatureHorizontalPosition'))
+            newTemplate.write(name)
+        elif '{{miniatureVerticalPosition}}' in line:
+            name = line.replace('{{miniatureVerticalPosition}}', values.get('miniatureVerticalPosition'))
+            newTemplate.write(name)
+        elif '{{miniatureLeftCutPercentage}}' in line:
+            name = line.replace('{{miniatureLeftCutPercentage}}', values.get('miniatureLeftCutPercentage'))
+            newTemplate.write(name)
+        elif '{{miniarureRightCutPercentage}}' in line:
+            name = line.replace('{{miniarureRightCutPercentage}}', values.get('miniarureRightCutPercentage'))
+            newTemplate.write(name)
         elif '{{endCorte}}' in line:
             name = line.replace('{{endCorte}}', values.get('endCorte'))
             newTemplate.write(name)
         else:
             newTemplate.write(line)
     return True
+
+def edit_video(newVideo: str, nameProject: str, loading,  miniature: dict = None):
+    if newVideo == '':
+        messagebox.showerror('Error', 'Selecciona un archivo')
+        return
+    if nameProject == '':
+        messagebox.showerror('Error', 'Ingresar nombre de proyecto')
+        return
+    nameProject = nameProject.lower().replace(' ', '-')
+    nameFolder = f"projects/{nameProject}"
+    if os.path.isdir(nameFolder) is not True:
+        os.makedirs(nameFolder.lower())
+
+    loading.set('procesando...')
+
+    templateAdobe = open('./base.xml')
+    nameFileXml = f"./{nameFolder}/{nameProject}.xml"
+    ourputAdobe = open(nameFileXml, 'w')
+    video = VideoFileClip(newVideo)
+    nameAudio = f"./{nameFolder}/{nameProject}.mp3"
+    video.audio.write_audiofile(nameAudio)
+
+    duration = video.duration * 60
+    videoName = video.filename
+    baseVideo = os.path.realpath(videoName).replace('\\', '/').split(':')
+    routeVideo = f"file://localhost/{baseVideo[0]}%3a{baseVideo[1]}"
+    baseAudio = os.path.realpath(nameAudio).replace('\\', '/').split(':')
+    routeAudio = f"file://localhost/{baseAudio[0]}%3a{baseAudio[1]}"
+    # routeProject.set(os.path.realpath(nameFolder))
+
+    y, sr = librosa.load(nameAudio)
+
+    tik = 254016000000
+    corteArrStart, startCorte = actions.getTimeStart(y, sr)
+    corteArrEnd, endCorte = actions.getTimeEnd(y, sr)
+
+    startFrame = float(len(corteArrStart) / sr) * 60
+    tiksStart = float(startCorte * tik)
+    tiksAudioEnd = float(endCorte * tik) - tiksStart
+    endCorte = ((len(y) - (len(corteArrEnd) + len(corteArrStart))) / sr) * 60
+
+    values = {
+        "videoName": videoName,
+        "routeVideo": routeVideo,
+        "routeAudio": routeAudio,
+        "startFrame": f"{startFrame}",
+        "tiksStart": f"{tiksStart}",
+        "endCorte": f"{endCorte}",
+        "miniatureScale": miniature.get('scale'),
+        "miniarureRightCutPercentage": miniature.get('right_cut_percentage'),
+        "miniatureLeftCutPercentage": miniature.get('left_cut_percentage'),
+        "miniatureVerticalPosition": miniature.get('vertical_position'),
+        "miniatureHorizontalPosition": miniature.get('horizontal_position'),
+    }
+    finish = actions.fill_template(templateAdobe, ourputAdobe, values)
+    if finish:
+        loading.set('terminado')
+        messagebox.showinfo('Correcto', 'Configuracion de proyecto terminada')
